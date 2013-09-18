840e4eec2d211e5942596cdc60419bba24608682
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-18 08:56:29 -0400

    new project set-up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opentable::Application.config.secret_key_base = 'a12a06dc876c9ba8e4e09f28d07471421ce1c4fdedbe44f16bf3b70e6dcd9d55ce8eeb2a8913128b249371f41f42f7f0bdfffb1c792f96a5c3b4103896db6fe9'
