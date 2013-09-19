bb0f8f2d6b80ccc4c17a303136a0cade913dedd2
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-19 16:29:12 -0400

    create reservation table and associations to user and restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,7 @@ class Restaurant < ActiveRecord::Base
 	validates :name, :phone_number, :summary, :address, :city, :province, :postal_code, :website, presence: true
 	validates :email, format: { with: /(\S+)@(\S+)/ }, presence: true
 	validates :max_seats, numericality: true
+
+	has_many :reservations
+	has_many :users, :through => :reservations
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 	validates_presence_of :password, :on => :create
+
+	has_many :reservations
+	has_many :restaurants, :through => :reservations
 end
