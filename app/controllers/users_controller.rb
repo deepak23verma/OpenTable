74e910f24ef329a3395754469b7c1e14e579ffa0
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-19 08:59:37 -0400

    user controller actions defined

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,26 +4,48 @@ class UsersController < ApplicationController
 	end
 
 	def show
+		@user = User.find(params[:id])
 	end
 
 	def edit
+		@user = User.find(params[:id])
 	end
 
 	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:notice] = "#{@user.first_name}'s profile successfully updated"
+			redirect_to @user
+		else
+			render: edit
+		end
 	end
 
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:notice] = "#{@user.first_name}, Welcome to OpenTable! Bon Appetit!"
+			redirect_to @user
+		else
+			render :new
+		end
 	end
 
 	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:alert] = "User deleted!"
+		redirect_to restaurants_path
 	end
 
 private
 	
 	def user_params
+		params.require(:user).permit(:first_name, :last_name, :password_digest, :user_type, :email, :phone_number)
 	end
-	
+
 end
