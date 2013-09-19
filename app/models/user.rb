db963318a0c9654a6c7b668ead4b56e245d3dc25
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-18 22:33:13 -0400

    validate user columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+	validates :first_name, :last_name, :password_digest, :user_type, :phone_number, presence: true
+	validates :email, format: { with: /(\S+)@(\S+)/ }, presence: true
 end
