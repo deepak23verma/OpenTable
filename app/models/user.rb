3f5a97b27d9b5af69b50e690d98bf51c541f5bfe
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-19 15:42:28 -0400

    add bycrpt gem and update user model validation for password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 	validates :first_name, :last_name, :password_digest, :phone_number, presence: true
 	validates :email, format: { with: /(\S+)@(\S+)/ }, presence: true
+
+	has_secure_password
 end
