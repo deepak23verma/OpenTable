e7fd2cff0c422598b5ca52108726f3a8907b5394
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-20 23:45:11 -0400

    add check_availability method to be run before new reservations are commited

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Reservation do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let!(:reservation) {FactoryGirl.create(:reservation)}
+
+	it "should return the requested reservation date" do
+	  expect(requested_time(:reservation)).to be "2013-09-19 16:17:48"
+	end
+
 end
